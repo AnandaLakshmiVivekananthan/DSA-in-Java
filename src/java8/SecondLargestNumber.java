dcb94e32bb90bc59b4899517d918b5fe860207e7
AnandaLakshmiVivekananthan <anandalakshmi2811@gmail.com>
2025-03-18 21:58:00 +0530

    PracticeJ8Problems

---
+++ src/java8/SecondLargestNumber.java
@@ -0,0 +1,25 @@
+package java8;
+
+/**
+ * @author Ananda Lakshmi Vivekananthan
+ */
+public class SecondLargestNumber {
+
+	public static void main(String[] args) {
+
+		int[] nums = {2,34,12,78,90};
+		int largest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
+
+		for(int num : nums){
+			if(num > secondLargest){
+				secondLargest = largest;
+				if(num > largest){
+					largest = num;
+				}
+			}
+		}
+		System.out.println("second largest:"+secondLargest);
+	}
+
+}
